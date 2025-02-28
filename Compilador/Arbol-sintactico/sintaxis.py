bd4dcdf46db5df9c866afb53a37dd4de9b40f8ee
JoSantana0005 <joseasantana05@gmail.com>
2025-02-27 23:07:18 -0400

    I have defined the class program, members, modify class, class, visibility

--- Compilador/Arbol-sintactico/sintaxis.py
+++ Compilador/Arbol-sintactico/sintaxis.py
@@ -1,55 +1,101 @@
 from abc import abstractmethod, ABCMeta
 
 ### Defincion de la clase Programa
-class Programa(metaclass=ABCMeta):
+class Program(metaclass=ABCMeta):
     @abstractmethod
-    def acceder(self,visitor):
+    def access(self,visitor):
         pass
 
-class ProgramaCompilador(Programa):
+class ProgramCompiler(Program):
     def __init__(self, clase):
         self.clase = clase
     
-    def acceder(self,visitor):
+    def access(self,visitor):
         return visitor.visitarPrograma(self)
 
 ### Definicion de la clase Clase
 class Class(metaclass=ABCMeta):
     @abstractmethod
-    def acceder(self,visitor):
+    def access(self,visitor):
         pass
 
 class ClassExtends(Class):
-    def __init__(self,visibilidad, claseModificar, ID_NOMECLASS, ID_NOMEEXTENDS, miembros):
-        self.visibilidad = visibilidad
+    def __init__(self,visibility, claseModificar, ID_NOMECLASS, ID_NOMEEXTENDS, members):
+        self.visibility = visibility
         self.claseModificar = claseModificar
         self.ID_NOMECLASS = ID_NOMECLASS
         self.ID_NOMEEXTENDS = ID_NOMEEXTENDS
-        self.miembros = miembros
+        self.members = members
     
-    def acceder(self,visitor):
+    def access(self,visitor):
         return visitor.VisitCClassExtends(self)
 
 class ClassDefault(Class):
-    def __init__(self,visibilidad, claseModificar, ID_NOMECLASS, miembros):
-        self.visibilidad = visibilidad
+    def __init__(self,visibility, claseModificar, ID_NOMECLASS, members):
+        self.visibility = visibility
         self.claseModificar = claseModificar
         self.ID_NOMECLASS = ID_NOMECLASS
-        self.miembros = miembros
+        self.members = members
 
-    def acceder(self,visitor):
+    def access(self,visitor):
         return visitor.VisitCClassDefault(self)
 
 class ClassImplements(Class):
-    def __init__(self,visibilidad, claseModificar, ID_NOMECLASS, miembros):
-        self.visibilidad = visibilidad
+    def __init__(self,visibility, claseModificar, ID_NOMECLASS, members):
+        self.visibility = visibility
         self.claseModificar = claseModificar
         self.ID_NOMECLASS = ID_NOMECLASS
-        self.miembros = miembros
+        self.members = members
 
-    def acceder(self,visitor):
+    def access(self,visitor):
         return visitor.VisitCClassImplements(self)
     
 ### Definicion de las clase visibilidad
-class visibilidad(metaclass=ABCMeta):
+class visibility(metaclass=ABCMeta):
+    @abstractmethod
+    def access(self,visitor):
+        pass
+
+class visibilityConcrete(visibility):
+    def __init__(self,visibilitys):
+        self.visibilitys = visibilitys
     
+    def access(self, visitor):
+        return visitor.VisitVisibilityConcrete(self)
+
+### definicion de la clase ClaseModificar
+class ClassModify(metaclass=ABCMeta):
+    @abstractmethod
+    def access(self,visitor):
+        pass
+
+class ClassModifyConcrete(ClassModify):
+    def __init__(self, classModifys):
+        self.classModifys = classModifys
+    
+    def access(self, visitor):
+        return visitor.VisitClassModifyConcrete(self)
+
+### Definiciond de la clase Miembros
+class Members(metaclass=ABCMeta):
+    @abstractmethod
+    def access(self,visitor):
+        pass
+
+class MembersUni(Members):
+    def __init__(self, member):
+        self.member = member
+    
+    def access(self,visitor):
+        return visitor.VisitMembersUni(self)
+
+class MembersMult(Members):
+    def __init__(self, member, members):
+        self.member = member
+        self.members = members
+    
+    def access(self,visitor):
+        return visitor.VisitMembersMult(self)
+
+### Definicion de la clase Miembro
+
