263e8f8e0dbfc7d9d32e167658860cb904fa404b
JoSantana0005 <joseasantana05@gmail.com>
2025-04-17 14:49:19 -0400

    Agregado al frotend

--- src/functions/lexer.py
+++ src/functions/lexer.py
@@ -1,174 +1,176 @@
+# Analizador Lexico
 import flet as ft
 import ply.lex as lex
 
+# Definición de las palabras reservadas
 reservadas = {
-   #TIPOS
-   'boolean': 'TYPE_BOOLEAN', 'int': 'TYPE_INT', 'float': 'TYPE_FLOAT', 'byte': 'TYPE_BYTE',
-   'double': 'TYPE_DOUBLE', 'char': 'TYPE_CHAR', 'String' : 'TYPE_STRING', 'long': 'TYPE_LONG',
-   'void': 'TYPE_VOID',
+    # Condicionales
+    'if': 'IF', "else": 'ELSE',
+    # Ciclos
+    'for': 'FOR', 'while': 'WHILE', 'do': 'DO',
+    # Funciones
+    'abstract': 'ABSTRACT', 'public': 'PUBLIC', 'private': 'PRIVATE', 'protected': 'PROTECTED',
+    'static': 'STATIC', 'void': 'VOID', 'return': 'RETURN', 'package': 'PACKAGE',
+    'import': 'IMPORT', 'class': 'CLASS', 'extends': 'EXTENDS', 'implements': 'IMPLEMENTS',
+    'new': 'NEW', 'this': 'THIS', 'super': 'SUPER',
 
-   #VISIBILIDADE
-   'public': 'PUBLIC', 'private': 'PRIVATE',
-   'default': 'DEFAULT', 'protected': 'PROTECTED',
- 
-   'abstract': 'ABSTRACT',
-   'assert': 'ASSERT',
-   'break': 'BREAK',
-   'case': 'CASE',
-   'catch': 'CATCH',
-   'class': 'CLASS',
-   'const': 'CONST',
-   'continue': 'CONTINUE',
-   'do': 'DO',
-   'else': 'ELSE',
-   'enum': 'ENUM',
-   'extends': 'EXTENDS',
-   'final': 'FINAL',
-   'finally': 'FINALLY',
-   'for': 'FOR',
-   'goto': 'GOTO',
-   'if': 'IF',
-   'implements': 'IMPLEMENTS',
-   'import': 'IMPORT',
-   'instanceof': 'INSTANCEOF',
-   'interface': 'INTERFACE',
-   'native': 'NATIVE',
-   'new': 'NEW',
-   'package': 'PACKAGE',
-   'return': 'RETURN',
-   'short': 'SHORT',
-   'static': 'STATIC',
-   'strictfp': 'STRICTFP',
-   'super': 'SUPER',
-   'switch': 'SWITCH',
-   'synchronized': 'SYNCHRONIZED',
-   'this': 'THIS',
-   'throw': 'THROW',
-   'throws': 'THROWS',
-   'transient': 'TRANSIENT',
-   'try': 'TRY',
-   'volatile': 'VOLATILE',
-   'while': 'WHILE'
+    # Tipos de datos
+    'int': 'INT', 'float': 'FLOAT', 'double': 'DOUBLE', 'char': 'CHAR',
+    'string': 'STRING', 'boolean': 'BOOLEAN',
+
+    # Manipulacion de errores
+    'try': 'TRY', 'catch': 'CATCH', 'finally': 'FINALLY', 'throw': 'THROW', 'assert': 'ASSERT',
+
+    # Otros
+    'export': 'EXPORT', 'native': 'NATIVE', 'synchronized': 'SYNCHRONIZED',
+    'volatile': 'VOLATILE', 'transient': 'TRANSIENT',
+    'default': 'DEFAULT', 'case': 'CASE','break': 'BREAK', 'continue': 'CONTINUE', 'instanceof': 'INSTANCEOF',
+    'final': 'FINAL', 'short': 'SHORT', 'long': 'LONG',
+    'byte': 'BYTE', 'interface': 'INTERFACE', 'switch': 'SWITCH'
 }
 
+# Lista de tokens
+tokens = list(reservadas.values()) + [
+    'ID', 'NUMERO', 'CADENA',
+    'ASIGNACION', 'SUMA', 'RESTA', 'MULTIPLICACION', 'DIVISION',
+    'MODULO', 'INCREMENTO', 'DECREMENTO',
+    'NOT', 'IGUAL', 'DISTINTO', 'MAYOR', 'MENOR', 'MAYOR_IGUAL',
+    'MENOR_IGUAL', 'PARENTESIS_ABIERTO', 'PARENTESIS_CERRADO',
+    'LLAVE_ABIERTA', 'LLAVE_CERRADA', 'CORCHETE_ABIERTO', 'CORCHETE_CERRADO',
+    'PUNTO_Y_COMA', 'COMA', 'PUNTO', 'DOSPUNTOS', 'AND_LOGICO',
+    'OR_LOGICO', 'OCTAL_NUMERO', 'HEX_NUMERO', 'BINARIO_NUMERO', 'TERNARIO',
+    'TRUE', 'FALSE', 'NULL', 'AND_BIT', 'OR_BIT', 'XOR_BIT',
+    'NOT_BIT', 'DESPLAZAMIENTO_IZQUIERDO', 'DESPLAZAMIENTO_DERECHO',
+    'DESPLAZAMIENTO_IZQUIERDO_ASIGNACION', 'DESPLAZAMIENTO_DERECHO_ASIGNACION',
+    'ASIGNACION_SUMA', 'ASIGNACION_RESTA','ASIGNACION_MULTIPLICACION', 'ASIGNACION_DIVISION', 'ASIGNACION_MODULO',
+    'UMENOS', 'UMAS'
+]
 
-
-tokens = ['INT_NUMBER', 'FLOAT_NUMBER', 'BYTE_NUMBER', 'DOUBLE_NUMBER', 'CHAR', 'STRING', 'LONG_NUMBER',
-         'BITWISE_XOR_EQ', 'BITWISE_OR_EQ', 'BITWISE_AND_EQ', 'BITWISE_XOR', 'BITWISE_NOT', 'BITWISE_OR', 'BITWISE_AND',
-          'EQUAL', 'POT', 'LPAREN', 'RPAREN', 'COMMA', 'DOT', 'LCHAV', 'RCHAV', 'SEMICOLON', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQ', 
-          'NEQ', 'LT', 'GT', 'LEQ', 'GEQ', 'AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT', 'URSHIFT', 'PLUS_EQ', 'MINUS_EQ',
-          'TIMES_EQ', 'DIVIDE_EQ', 'MOD_EQ',  'LSHIFT_EQ', 'RSHIFT_EQ', 'URSHIFT_EQ', 'ID',
-          'RBRACKET', 'LBRACKET', 'HEXA_NUMBER', 'OCTAL_NUMBER', 'BIN_NUMBER', 'INCREMENT', 'DECREMENT', 'TERNARY', 'MODULE'] + list(reservadas.values())
-
-
-t_URSHIFT_EQ = r'>>>='
-t_LSHIFT_EQ  = r'<<='
-t_RSHIFT_EQ  = r'>>='
-t_URSHIFT  = r'>>>'
-t_LSHIFT  = r'<<'
-t_RSHIFT = r'>>'
-t_LEQ = r'<='
-t_GEQ = r'>='
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_INCREMENT = r'\+\+'
-t_DECREMENT = r'--'
-t_PLUS_EQ = r'\+='
-t_MINUS_EQ = r'-='
-t_TIMES_EQ = r'\*='
-t_DIVIDE_EQ = r'/='
-t_MOD_EQ = r'%='
-t_MODULE = r'%'
-t_BITWISE_AND_EQ = r'&='
-t_BITWISE_OR_EQ = r'\|='
-t_BITWISE_XOR_EQ = r'^='
-t_EQ = r'=='
-t_NEQ = r'!='
-t_EQUAL = r'='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACKET  = r'\['
-t_RBRACKET  = r'\]'
-t_COMMA = r','
-t_DOT = r'\.'
-t_LCHAV = r'{'
-t_RCHAV = r'}'
-t_SEMICOLON = r';'
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LT = r'<'
-t_GT = r'>'
+# Definición de los tokens
+t_NUMERO = r'\d+(\.\d+)?'  # Números enteros y decimales
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_MULTIPLICACION = r'\*'
+t_DIVISION = r'/'
+t_MODULO = r'%'
+t_ASIGNACION = r'='
+t_INCREMENTO = r'\+\+'
+t_DECREMENTO = r'--'
 t_NOT = r'!'
-t_TERNARY = r'\?'
-t_BITWISE_AND = r'&'
-t_BITWISE_OR = r'\|'
-t_BITWISE_XOR = r'\^'
-t_BITWISE_NOT = r'~'
+t_IGUAL = r'=='
+t_DISTINTO = r'!='
+t_MAYOR = r'>'
+t_MENOR = r'<'
+t_MAYOR_IGUAL = r'>='
+t_MENOR_IGUAL = r'<='
+t_PARENTESIS_ABIERTO = r'\('
+t_PARENTESIS_CERRADO = r'\)'
+t_LLAVE_ABIERTA = r'\{'
+t_LLAVE_CERRADA = r'\}'
+t_CORCHETE_ABIERTO = r'\['
+t_CORCHETE_CERRADO = r'\]'
+t_PUNTO_Y_COMA = r';'
+t_COMA = r','
+t_PUNTO = r'\.'
+t_DOSPUNTOS = r':'
+t_UMENOS = r'\-'
+t_UMAS = r'\+'
+t_AND_LOGICO = r'&&'
+t_OR_LOGICO = r'\|\|'
+t_TERNARIO = r'\?\:'
+t_TRUE = r'true'
+t_FALSE = r'false'
+t_NULL = r'null'
+t_AND_BIT = r'&'
+t_OR_BIT = r'\|'
+t_XOR_BIT = r'\^'
+t_NOT_BIT = r'~'
+t_DESPLAZAMIENTO_IZQUIERDO = r'<<'
+t_DESPLAZAMIENTO_DERECHO = r'>>'
+t_DESPLAZAMIENTO_IZQUIERDO_ASIGNACION = r'<<='
+t_DESPLAZAMIENTO_DERECHO_ASIGNACION = r'>>='
+t_ASIGNACION_SUMA = r'\+='
+t_ASIGNACION_RESTA = r'-='
+t_ASIGNACION_MULTIPLICACION = r'\*='
+t_ASIGNACION_DIVISION = r'/='
+t_ASIGNACION_MODULO = r'%='
 
-
-def t_comments_1(t):
-   r'//.*\n'
-   t.lexer.lineno += 1
-
-def t_comments_2(t):
-  r'/\*(.|\n)*?\*/'
-  t.lexer.lineno += len(t.value)  
-
-
-def t_BIN_NUMBER(t):
-   r'0b[01]+'
-   t.value = int(t.value, base=2)
-   return t
 
 def t_OCTAL_NUMBER(t):
- r'0[0-7]+'
- t.value = int(t.value, base=8)
- return t
+    r'0[0-7]+'
+    t.type = 'OCTAL_NUMERO'
+    t.value = int(t.value, 8)
+    return t
 
-def t_HEXA_NUMBER(t):
-   r'0(x|X)[a-fA-F0-9]+'
-   t.value = int(t.value, base=16)
-   return t
+def t_HEX_NUMBER(t):
+    r'0[xX][0-9a-fA-F]+'
+    t.type = 'HEX_NUMERO'
+    t.value = int(t.value, 16)
+    return t
 
-def t_FLOAT_NUMBER(t):
-   r'\d+\.\d+(f|F)' #Problema com ex: 40.5f
-   t.value = float(t.value)
-   return t
+def t_BINARIO_NUMERO(t):
+    r'0[bB][01]+'
+    t.type = 'BINARIO_NUMERO'
+    t.value = int(t.value, 2)
+    return t
 
-def t_DOUBLE_NUMBER(t):
-   r'\d+\.\d+'
-   t.value = float(t.value) 
-   return t
+def t_INT(t):
+    r'\d+'
+    t.type = 'NUMERO'
+    t.value = int(t.value)  # Convertir a entero
+    return t
 
-def t_INT_NUMBER(t):
-   r'\d+'
-   t.value = int(t.value)
-   return t
+def t_MAIN(t):
+    r'main'
+    t.type = 'ID'
+    return t
 
-def t_CHAR(t):
-  r"'(.|\n)?'"
-  return t
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.type = 'NUMERO'
+    t.value = float(t.value)  # Convertir a float
+    return t
+
+def t_DOUBLE(t):
+    r'\d+\.\d+[dD]'
+    t.type = 'NUMERO'
+    t.value = float(t.value[:-1])  # Convertir a float y eliminar la 'd' o 'D'
+    return t
+
 
 def t_STRING(t):
-  r'"(.|\n)*?"'
-  return t
+    r'\"([^\\\"]|\\.)*\"'
+    t.type = 'CADENA'
+    t.value = t.value[1:-1]  # Eliminar las comillas
+    return t
 
 def t_ID(t):
-   r'[a-zA-Z_][a-zA-Z_0-9.*]*'
-   t.type = reservadas.get(t.value,'ID')
-   return t
+    r'[a-zA-Z_\u00A0-\uFFFF][a-zA-Z0-9_\u00A0-\uFFFF]*'
+    t.type = reservadas.get(t.value, 'ID')  # Verifica si es una palabra reservada
+    return t
 
-def t_newline(t):
-   r'\n+'
-   t.lexer.lineno += len(t.value)
-t_ignore = ' \t'
+def t_COMENTARIO_1(t):
+    r'//.*\n'
+    t.lexer.lineno += 1  # Aumentar el número de línea
+
+def t_COMENTARIO_MULTILINEA(t):
+    r'/\*.*?\*/'
+    t.lexer.lineno += t.value.count('\n')  # Aumentar el número de línea
+    t.value = t.value.replace('\n', '')  # Eliminar saltos de línea en el comentario
+
+def t_newLine(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)  # Aumentar el número de línea
+
+t_ignore = ' \t'  # Ignorar espacios y tabulaciones
 
 def t_error(t):
-   print("Illegal character '%s'" % t.value[0])
-   t.lexer.skip(1)
+    print(f"Caracter no reconocido: {t.value[0]}")
+    t.lexer.skip(1)
 
+# Define el lexer (asegúrate de que esté configurado previamente)
+lexer = lex.lex()
 def proceso_lexer(e):
     """
     Procesa el texto de un TextField utilizando un lexer y muestra los tokens.
@@ -180,8 +182,7 @@ def proceso_lexer(e):
             print("El TextField está vacío.")
             return
 
-        # Define el lexer (asegúrate de que esté configurado previamente)
-        lexer = lex.lex()
+        
 
         # Procesa el texto del TextField
         lexer.input(text_field_value)
@@ -206,5 +207,3 @@ def proceso_lexer(e):
     except Exception as e:
         print(f"Ocurrió un error al procesar el texto: {e}")
         return
-
-
