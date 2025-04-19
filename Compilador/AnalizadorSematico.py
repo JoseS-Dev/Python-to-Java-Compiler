15cb97672e4427e929b08b48e9eb9eab89e93ea0
JoSantana0005 <joseasantana05@gmail.com>
2025-04-19 11:29:22 -0400

    Comienzo el analizador semantico

--- Compilador/AnalizadorSematico.py
+++ Compilador/AnalizadorSematico.py
@@ -1,1 +1,24 @@
-# Solo Faltas tu
+class AnalizadorSemantico:
+    def __init__(self):
+        self.tabla_simbolos = {
+            'global': {
+                'clases': {},
+                'variables': {},
+                'funciones': {}
+            }
+        }
+        self.current_scope = 'global'
+        self.current_class = None
+        self.current_method = None
+        self.errors = []
+        self.Warning = []
+        self.type_system = {
+            'primitive': [ 'int', 'float', 'double', 'char', 'boolean', 'string', 'short', 'long', 'byte'],
+            'compatible': {
+                'int': ['long', 'short', 'byte'],
+                'float': ['double'],
+                'double': ['float'],
+                'long': ['int', 'short', 'byte'],
+                'short': ['byte']
+            }
+        }
